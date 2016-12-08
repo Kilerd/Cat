5f27f78a96c1abee8be710b5d1dcf7f2e2556309
Kilerd Chan <blove694@gmail.com>
2016-12-08 17:01:07 +0800

    git pages

--- typecho/cat/footer.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<footer>
-    Powered with <span><a href="http://typecho.org/">Typecho</a></span>, Theme by <span><a href="https://github.com/Kilerd/Cat">Cat</a></span><?php $this->options->analyticsCode() ?>.
-</footer>
-</body>
-</html>
